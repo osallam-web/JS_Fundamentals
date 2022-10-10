01abc00704dd4f524ede7903eee80098b36377dd
Omar Sallam <omar.sallam@gmail.com>
2022-10-10 18:02:02 -0400

    Added Iteration example file

---
+++ 1.Basics/4.Iteration.js
@@ -0,0 +1,6 @@
+let fruits = [`dates`, `bannanas`, `grapes`, `oranges`, `watermelon`, `peaches`];
+
+console.log("Our fruit selection is: ");
+for (let index = 0; index < fruits.length; index++) {
+   console.log(`${fruits[index]}`);
+};
