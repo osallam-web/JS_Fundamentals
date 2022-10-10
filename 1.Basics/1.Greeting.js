1663ef7072bf7e142eb97818e1c07934a76fa6d4
Omar Sallam <omar.sallam@gmail.com>
2022-10-10 10:37:46 -0400

    Added 2 files

---
+++ 1.Basics/1.Greeting.js
@@ -0,0 +1,26 @@
+// Code to use prompts in Node
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Greeting and prompting for name and feedback
+rl.question('Hello. What is your name ? ', function (name) {
+console.log("It's nice to meet you,", name)
+rl.question('Are you enjoying the course? Y for Yes and N for No ', function (answer) {
+  if (answer === 'Y') {
+  console.log(`${name}, is in the course and his answer is Yes`);
+  }
+  else {
+    console.log(`${name}, is in the course and he is not enjoying the course sadly :()`);
+  }
+  rl.close();
+});
+});
+
+// Thanking and Exiting node
+rl.on('close', function () {
+  console.log('\nThanks for participating!\nBye now :)');
+  process.exit(0);
+});
---
+++ 1.Basics/2.favouriteCity.js
@@ -0,0 +1,9 @@
+// Function to return a favourite city
+
+function favCity(cityName){
+  console.log(`One of my favourite cities is: ${cityName}`)
+}
+
+favCity("Cairo")
+favCity("Medina")
+favCity("Boston")
